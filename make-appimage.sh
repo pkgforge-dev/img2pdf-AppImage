117e27a7b92f7df6c23789e9dc7bce5f96df7e4a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-07-17 20:05:52 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,34 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q img2pdf | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DEPLOY_PYTHON=1
+export ICON=DUMMY
+export DESKTOP=DUMMY
+export MAIN_BIN=img2pdf
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/img2pdf-gui*      \
+	/usr/lib/libtk8*.so*       \
+	/usr/lib/libtcl*.so        \
+	/usr/lib/tk8*              \
+	/usr/lib/tcl8*             \
+	/usr/lib/libwebp.so*       \
+	/usr/lib/libimagequant.so* \
+	/usr/lib/libopenjp2.so*    \
+	/usr/lib/libjpeg.so*       \
+	/usr/lib/libtiff.so*       \
+	/usr/lib/libpng*.so*       \
+	/usr/lib/libwebp.so*       \
+	/usr/lib/libjbig.so*       \
+	/usr/lib/libp11-kit.so*    \
+	/usr/lib/libqpdf.so*       \
+	/usr/lib/libglib-*.so*
 
 # Additional changes can be done in between here
 
